093d5820c43e465171f8c2048a0feee373c3f83c
Hermanowicz Tomasz <thermanowicz@outlook.com>
2022-04-23 11:30:36 +0200

    hundler = Binary Name

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+
 	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+type Response struct {
+	Res string
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	ret := fmt.Sprintf("Hello, %s!", name.Name)
+	r, _ := json.Marshal(Response{ret})
+	return string(r), nil
+}
+
 func main() {
-	fmt.Println("Hello world from test print.")
+	lambda.Start(HandleRequest)
 }
